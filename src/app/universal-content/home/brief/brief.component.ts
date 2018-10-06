79fd7946841062df8d007ae92a318bf1c328b434
singhania94 <singhania.aditya94@gmail.com>
2018-10-06 08:56:05 +0530

    adding introduction, contact and brief to home page

---
+++ src/app/universal-content/home/brief/brief.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-brief',
+  templateUrl: './brief.component.html',
+  styleUrls: ['./brief.component.css']
+})
+export class BriefComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
