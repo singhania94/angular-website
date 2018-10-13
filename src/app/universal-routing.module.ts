b47ce0900d101fd1a4f3a4866453a440035cd070
singhania94 <singhania.aditya94@gmail.com>
2018-10-13 09:09:47 +0530

    draft one complete of about section

--- src/app/universal-routing.module.ts
+++ src/app/universal-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})
   ],
   exports: [
     RouterModule
